c872c66a98e28a32310adb697ba1599149564ce3
Sebaxdenc <samedinac@eafit.edu.co>
2025-08-21 12:51:10 -0500

    creacion del proyecto con spring en el initializer y modificacion del gitignore

---
+++ src/main/java/grupo4/demo/CabaProApplication.java
@@ -0,0 +1,13 @@
+package grupo4.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabaProApplication.class, args);
+	}
+
+}
