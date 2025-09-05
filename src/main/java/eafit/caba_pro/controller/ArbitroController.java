ee4fac2a86dd6d820affabd62a9eb8e411efae87
niosto <nicolasospinat@gmail.com>
2025-09-05 18:08:18 -0500

    Fix front nav bar

--- src/main/java/eafit/caba_pro/controller/ArbitroController.java
+++ src/main/java/eafit/caba_pro/controller/ArbitroController.java
@@ -1,14 +1,31 @@
 package eafit.caba_pro.controller;
+import eafit.caba_pro.service.UsuarioService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/arbitro")
 public class ArbitroController {
+    @Autowired
+    UsuarioService usuarioService;
+
     @GetMapping
     public String dashboard(){
+        //return "dsa";
         return "arbitro/dashboard";
+    }
+
+    // Ejemplo de como obtener el nombre de usuario de la actual sesión
+    @GetMapping("/prueba")
+    public String ejemplo(Model model){
+        String username = usuarioService.getCurrentUsername();
+        model.addAttribute("username", username);
+
+        return "ejemplo";
     }
 }
--- src/main/java/eafit/caba_pro/service/UsuarioService.java
+++ src/main/java/eafit/caba_pro/service/UsuarioService.java
@@ -4,8 +4,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -38,4 +40,18 @@ public class UsuarioService implements UserDetailsService{
         return user;
     }
 
+    public String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+
+            if (principal instanceof UserDetails) {
+                return ((UserDetails) principal).getUsername();
+            } else {
+                return principal.toString(); // For cases where principal isn't UserDetails
+            }
+        }
+        return null;
+    }
 }
