2c8656993177b113871a6b9539d6b5fd3526fc04
Sebaxdenc <samedinac@eafit.edu.co>
2025-11-11 11:47:42 -0500

    añadi traducción al clima

--- src/main/java/eafit/caba_pro/service/ClimaService.java
+++ src/main/java/eafit/caba_pro/service/ClimaService.java
@@ -1,19 +1,25 @@
 package eafit.caba_pro.service;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
+import java.util.Locale;
 import java.util.Map;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ClimaService {
 
     private final RestTemplate restTemplate = new RestTemplate();
+    private final MessageSource messageSource;
 
     public Map<String, Object> getWeather(double latitude, double longitude, LocalDate date) {
         String url = UriComponentsBuilder.fromUriString("https://api.open-meteo.com/v1/forecast")
@@ -29,27 +35,29 @@ public class ClimaService {
     }
 
     public String mapCode(Integer code) {
-        String clima;
+        Locale locale = LocaleContextHolder.getLocale();
+        String key;
 
         if (code == 0) {
-            clima = "Despejado";
+            key = "weather.clear";
         } 
         else if (code >= 1 && code <= 3) {
-            clima = "Parcialmente nublado";
+            key = "weather.partly_cloudy";
         } 
         else if ((code >= 45 && code <= 48) || (code >= 51 && code <= 67) || (code >= 80 && code <= 82)) {
-            clima = "Lluvioso";
+            key = "weather.rainy";
         } 
         else if ((code >= 95 && code <= 99)) {
-            clima = "Tormenta";
+            key = "weather.stormy";
         } 
         else if ((code >= 71 && code <= 77) || (code >= 85 && code <= 86)) {
-            clima = "Nevado";
+            key = "weather.snowy";
         } 
         else {
-            clima = "Desconocido";
+            key = "weather.unknown";
         }
-        return clima;
+        
+        return messageSource.getMessage(key, null, locale);
     }
 
 }
